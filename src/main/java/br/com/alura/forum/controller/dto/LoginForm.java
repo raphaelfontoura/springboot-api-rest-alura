51db5de482a9a52f0d57e4d629cd41be68305dab
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-08-04 12:13:14 -0300

    Add jjwt dependency. Add endpoint to auth. Create LoginForm dto

---
+++ src/main/java/br/com/alura/forum/controller/dto/LoginForm.java
@@ -0,0 +1,19 @@
+package br.com.alura.forum.controller.dto;
+
+
+public class LoginForm {
+    private String email;
+    private String senha;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}

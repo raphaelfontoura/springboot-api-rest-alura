99b0ac838bdfc85dba8c18f8689052fea7f8d1d8
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-08-06 17:56:11 -0300

    Refactor tests. Make test to authController

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -4,10 +4,12 @@ package br.com.alura.forum;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class ForumApplicationTests {
 
 	@Test
